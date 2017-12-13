5887ca43b8b686ac139b66595cbea63e83e31a51
nikhilbhatia7 <nikhil.bhatia@monotype.com>
2017-12-13 16:09:20 +0530

    Added Chrome Storage Api

---
+++ clock.js
@@ -0,0 +1,55 @@
+var hours = 0;
+var minutes = 0;
+var seconds = 0;
+
+var time = new Date();
+hours = time.getHours()
+minutes = time.getMinutes()
+seconds = time.getSeconds()
+
+showTime(hours, minutes, seconds);
+
+function showTime (hours, minutes, seconds) {
+	// console.log(hours + " : " + minutes + " : " + seconds + "\n");
+	if (hours < 10) {
+		hours = '0' + (hours%12)
+	}
+	if (minutes < 10) {
+		minutes = '0' + minutes
+	}
+	if (seconds < 10) {
+		seconds = '0' + seconds
+	}
+	const time = hours + " : " + minutes + " : " + seconds;
+	document.getElementById('timer').innerHTML = time
+}
+
+function updateMinutes() {
+	if (minutes == 59) {
+		minutes = 0;
+		updateHours();
+	} else {
+		minutes++;
+	}
+}
+
+function updateHours() {
+	if (hours == 12) {
+		hours = 1;
+	} else {
+		hours++;
+	}
+}
+
+function calculateTime(){
+	if (seconds == 59) {
+		seconds = 0;
+		updateMinutes();
+	} else {
+		seconds++;
+	}
+	showTime(hours, minutes, seconds);
+}
+
+setInterval(calculateTime, 1000);
+calculateTime();
---
+++ image-fetcher.js
@@ -0,0 +1,55 @@
+console.log('Fetching the images')
+
+function getRandomImage(minimum, maximum) {
+	return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+}
+
+
+var activities = document.getElementById("category");
+activities.addEventListener("change", storeInChromeStorage);
+function storeInChromeStorage()
+{
+  var a = document.getElementById('category').value;
+  //console.log(a);
+  chrome.storage.sync.set({'storedQuery': a});
+  callAPI();
+}
+
+
+function reqListener () {
+  let response = JSON.parse(this.responseText)
+  if (!response.totalHits || (response.totalHits === 0)) {
+  	console.log('No images found')
+  } else {
+  	let index = getRandomImage(0, response.hits.length - 1)
+  	console.log('Index: ', index, response.hits[index])
+  	let url = response.hits[index]['largeImageURL']
+  	console.log('URL: ', url)
+  	let backgroundImageEle = document.getElementById('background-image')
+  	backgroundImageEle.style.backgroundImage = "url('" + url + "')"
+
+  }
+}
+
+
+function callAPI() {
+  var oReq = new XMLHttpRequest();
+
+  var API_KEY = '7159995-77ef52f978ebf70313e64948f';
+  var URL = "";
+  chrome.storage.sync.get('storedQuery', function (result) {
+    var query = result.storedQuery;
+
+      // Make this query value wala option as selected
+
+    URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(query)+"&image_type=photo&pretty=true&per_page=50&response_group=high_resolution";
+    oReq.open("GET", URL);
+    oReq.send();
+  });
+
+  
+  oReq.addEventListener("load", reqListener);
+
+}
+
+callAPI();
