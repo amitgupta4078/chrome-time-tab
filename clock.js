8e9e7b3f94fc7a1554e3313ff2f6332633781036
Amit Gupta <amit.gupta@monotype.com>
2017-12-13 17:51:07 +0530

    Code refactor; greeting messages

--- clock.js
+++ clock.js
@@ -1,55 +1,74 @@
-var hours = 0;
-var minutes = 0;
-var seconds = 0;
+window.onload = function() {
+	let hours = 0;
+	let minutes = 0;
+	let seconds = 0;
 
-var time = new Date();
-hours = time.getHours()
-minutes = time.getMinutes()
-seconds = time.getSeconds()
+	let time = new Date();
+	const greetingEle = document.getElementById('greeting')
 
-showTime(hours, minutes, seconds);
+	hours = time.getHours()
+	minutes = time.getMinutes()
+	seconds = time.getSeconds()
 
-function showTime (hours, minutes, seconds) {
-	// console.log(hours + " : " + minutes + " : " + seconds + "\n");
-	if (hours < 10) {
-		hours = '0' + (hours%12)
-	}
-	if (minutes < 10) {
-		minutes = '0' + minutes
-	}
-	if (seconds < 10) {
-		seconds = '0' + seconds
-	}
-	const time = hours + " : " + minutes + " : " + seconds;
-	document.getElementById('timer').innerHTML = time
-}
-
-function updateMinutes() {
-	if (minutes == 59) {
-		minutes = 0;
-		updateHours();
-	} else {
-		minutes++;
-	}
-}
-
-function updateHours() {
-	if (hours == 12) {
-		hours = 1;
-	} else {
-		hours++;
-	}
-}
-
-function calculateTime(){
-	if (seconds == 59) {
-		seconds = 0;
-		updateMinutes();
-	} else {
-		seconds++;
-	}
 	showTime(hours, minutes, seconds);
+	setInterval(calculateTime, 1000);
+	calculateTime();
+
+	
+	function showTime (hours, minutes, seconds) {
+		// Set greeting message according to the time
+		if (hours < 12) {
+			greetingEle.innerText = "Good Morning"
+		} else if (hours >= 12 && hours <= 16) {
+			greetingEle.innerText = "Good Afternoon"
+		} else if (hours > 16 && hours <= 18) {
+			greetingEle.innerText = "Good Evening"
+		} else {
+			greetingEle.innerText = "Good Night"
+		}
+
+		hours %= 12
+		if (hours < 10) {
+			hours = '0' + (hours)
+		}
+		if (minutes < 10) {
+			minutes = '0' + minutes
+		}
+		if (seconds < 10) {
+			seconds = '0' + seconds
+		}
+		const time = hours + " : " + minutes + " : " + seconds;
+		document.getElementById('timer').innerHTML = time
+	}
+
+	function updateMinutes() {
+		if (minutes == 59) {
+			minutes = 0;
+			updateHours();
+		} else {
+			minutes++;
+		}
+	}
+
+	function updateHours() {
+		if (hours == 12) {
+			hours = 1;
+		} else {
+			hours++;
+		}
+	}
+
+	function calculateTime(){
+		if (seconds == 59) {
+			seconds = 0;
+			updateMinutes();
+		} else {
+			seconds++;
+		}
+		showTime(hours, minutes, seconds);
+	}
+
+
 }
 
-setInterval(calculateTime, 1000);
-calculateTime();
+
--- image-fetcher.js
+++ image-fetcher.js
@@ -1,21 +1,22 @@
-console.log('Fetching the images')
+const activities = document.getElementById("category");
+activities.addEventListener("change", handleCategoryChange);
 
-function getRandomImage(minimum, maximum) {
-	return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+document.addEventListener("DOMContentLoaded", callAPI, false);
+
+function handleCategoryChange () {
+  storeInChromeStorage();
+  callAPI();
 }
 
-
-var activities = document.getElementById("category");
-activities.addEventListener("change", storeInChromeStorage);
 function storeInChromeStorage()
 {
-  var a = document.getElementById('category').value;
-  //console.log(a);
+  const a = activities.value;
   chrome.storage.sync.set({'storedQuery': a});
-  callAPI();
-
 }
 
+function getRandomImage(minimum, maximum) {
+  return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+}
 
 function reqListener () {
   let response = JSON.parse(this.responseText)
@@ -23,35 +24,24 @@ function reqListener () {
   	console.log('No images found')
   } else {
   	let index = getRandomImage(0, response.hits.length - 1)
-  	console.log('Index: ', index, response.hits[index])
   	let url = response.hits[index]['largeImageURL']
-  	console.log('URL: ', url)
   	let backgroundImageEle = document.getElementById('background-image')
   	backgroundImageEle.style.backgroundImage = "url('" + url + "')"
-
   }
 }
 
-
 function callAPI() {
-  var oReq = new XMLHttpRequest();
+  const oReq = new XMLHttpRequest();
 
-  var API_KEY = '7159995-77ef52f978ebf70313e64948f';
-  var URL = "";
+  const API_KEY = '7159995-77ef52f978ebf70313e64948f';
+  let URL = "";
   chrome.storage.sync.get('storedQuery', function (result) {
-    var query = result.storedQuery;
-
-      // Make this query value wala option as selected
+    const query = result.storedQuery;
+    activities.value = query
 
     URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(query)+"&image_type=photo&pretty=true&per_page=50&response_group=high_resolution";
     oReq.open("GET", URL);
     oReq.send();
   });
-
-  
   oReq.addEventListener("load", reqListener);
-
 }
-
-callAPI();
-
